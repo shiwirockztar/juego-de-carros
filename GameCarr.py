32ece68d090e4c4ec496af0719cf9dc5c04ad7a2
shiwirockztar <shiwirockztar@gmail.com>
2021-08-04 18:44:32 -0500

    agregando el dado

--- GameCarr.py
+++ GameCarr.py
@@ -2,3 +2,7 @@ import sys          #libreria para limpiar pantalla
 import time         #libreria para el uso del time.sleep()
 import random       #libreria para el uso de los numero aleatorios
 import os           #libreria para el uso del os.system('cls')
+
+print(random.randint(3, 9))
+time.sleep(1)
+os.system('cls')
