833ca3d4e493e82b6f134b5af4ff3383588a6ed4
shiwirockztar <shiwirockztar@gmail.com>
2021-08-04 19:15:56 -0500

    creacion de lista de jugadores y dado para el juego

--- GameCarr.py
+++ GameCarr.py
@@ -3,6 +3,17 @@ import time         #libreria para el uso del time.sleep()
 import random       #libreria para el uso de los numero aleatorios
 import os           #libreria para el uso del os.system('cls')
 
-print(random.randint(3, 9))
+
+lst=[] # lista de jugadores
+
+d='y' # variable para la Desicion de aceptar mas jugadores comienza como YES
+print("Bienvenido al juego de los carros\n")
+while d!='n':
+	N=input("Escriba el nombre de jugador \n")
+	lst.append(N)
+	d=input("Desea agregar otro jugador Y o N \n").lower() 
+	pass
+print(random.randint(0, 6)) #dado
+print(lst)
 time.sleep(1)
 os.system('cls')
